50be858f5a6bc8c60a2d5b96f143e5c5dda0a66a
Dave <62606500+FutureCCIE@users.noreply.github.com>
2021-04-15 10:05:00 -0400

    Create all_jobs.py

---
+++ all_jobs.py
@@ -0,0 +1,21 @@
+## Vulnerbility Investigation Automation (VIA)
+# Author: David Murphy
+# Date: 2020-08-21
+
+# importing all programs
+from vuln import *
+from all_sql import *
+from datetime import datetime
+now = datetime.now()
+today = now.strftime('20%y_%m_%d')
+
+# referencing all programs:
+vuln = vuln()
+all_sql = all_sql()
+
+#   Cisco psirt vulnerbilities
+#       * two seperate data stores: vulns & products for sql pri_key relating
+#
+psirt_data = vuln.get_bearer_token(f'cisco_vuln{today}.txt', f'cisco_products{today}.txt')
+cisco_vuln = all_sql.file_interpreter(f'cisco_vuln{today}.txt', f'cisco_vuln{today}')
+cisco_vulns = all_sql.file_interpreter(f'cisco_products{today}.txt', f'cisco_products{today}')
